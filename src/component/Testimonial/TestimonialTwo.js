bd12078e2e699963e0422c55c37321920797676d
LukeZvada <zvadamusic@gmail.com>
2021-01-21 23:38:28 -0600

    updated testimonial section

--- src/component/Testimonial/TestimonialTwo.js
+++ src/component/Testimonial/TestimonialTwo.js
@@ -15,7 +15,7 @@ class TestimonialTwo extends Component{
                         </div>
                         <div className="col-lg-6">
                             <div className="testimonial_slider_info">
-                                <SectionTitleTwo stitle="Customer feedback" btitle="What our customers say about John.."/>
+                                <SectionTitleTwo stitle="Testomonials" btitle=""/>
                                 <TestimonialSliderTwo/>
                             </div>
                         </div>
